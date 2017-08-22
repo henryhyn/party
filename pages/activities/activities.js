aacfd949979b48cc2b0eab27e0c2d9cc8b4e6313
何永能 <yongneng.he@dianping.com>
2017-08-22 16:21:36 +0800

    logs => activities

--- pages/activities/activities.js
+++ pages/activities/activities.js
@@ -2,14 +2,14 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    list: []
   },
   onLoad: function () {
     var me = this
     wx.request({
       url: 'https://kaiyuanshuwu.com/api/activities',
       success: function (res) {
-        me.setData({ logs: res.data.data.list || [] })
+        me.setData({ list: res.data.data.list || [] })
       }
     })
   }
