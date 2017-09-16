57b2ca37f10c1af6efd69b8406ebf5b091cc9633
何永能 <yongneng.he@dianping.com>
2017-09-16 17:21:54 +0800

    get user info on launch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { users } from './store/index'
 //app.js
 App({
   onLaunch: function () {
+    this.getUserInfo()
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
