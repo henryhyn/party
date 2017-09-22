356ae7b7c727a9fe25c7de105767ca2ffafdcb11
何永能 <yongneng.he@dianping.com>
2017-09-22 14:45:27 +0800

    storage

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ App({
   },
 
   getUserInfo: function (cb) {
+    this.globalData.userInfo = wx.getStorageSync('userInfo')
     if (this.globalData.userInfo) {
       typeof cb == "function" && cb(this.globalData.userInfo)
     } else {
       // 登录
       users.login(data => {
         this.globalData.userInfo = data
+        wx.setStorageSync('userInfo', data)
         typeof cb == "function" && cb(this.globalData.userInfo)
       })
     }
